0752f6859d3a27009455a9b598d9819541569843
Arefa Arefa <arefa@ltgpu2.informatik.uni-hamburg.de>
2023-06-20 14:27:16 +0200

    updated function

--- entitylinker/utils/get_neighbors.py
+++ entitylinker/utils/get_neighbors.py
@@ -7,10 +7,8 @@ from SPARQLWrapper import SPARQLWrapper, JSON, XML, N3, RDF
 sparql = SPARQLWrapper("https://dbpedia.org/sparql")
 
 def get_neighbors(entity):
-    # 1. for 1:N relations
-    sparql.setQuery(
-        """
-        PREFIX dbo: <http://dbpedia.org/ontology/>
+    query_prefix = """
+        PREFIX dbo: <http://dbpedia.org/ontology/> 
         PREFIX owl: <http://www.w3.org/2002/07/owl#>
         PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
@@ -22,9 +20,14 @@ def get_neighbors(entity):
         PREFIX dbpedia: <http://dbpedia.org/>
         PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 
+    """
+    # 1. for entity => node relations
+    sparql.setQuery(
+        query_prefix
+        +
+        """
         SELECT distinct ?p ?node WHERE{
             :""" + given_entity + """ ?p ?node .
-            ?node ?p :""" + given_entity + """ .
         }
         LIMIT 50
         """
@@ -33,7 +36,7 @@ def get_neighbors(entity):
     # set return type to JSON
     sparql.setReturnFormat(JSON)
 
-    # execcute sparql query and write result to results
+    # execute sparql query and write result to results
     results = sparql.query().convert()
 
     for result in results["results"]["bindings"]:
@@ -41,21 +44,11 @@ def get_neighbors(entity):
 
     print('\n\n')
 
-    # 2. for N:1 relations
+    # 2. for node => entity relations
     sparql.setQuery(
+        query_prefix
+        +
         """
-        PREFIX dbo: <http://dbpedia.org/ontology/>
-        PREFIX owl: <http://www.w3.org/2002/07/owl#>
-        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-        PREFIX dc: <http://purl.org/dc/elements/1.1/>
-        PREFIX : <http://dbpedia.org/resource/>
-        PREFIX dbpedia2: <http://dbpedia.org/property/>
-        PREFIX dbpedia: <http://dbpedia.org/>
-        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-
         SELECT distinct ?node ?p WHERE{
             ?node ?p :""" + given_entity + """ .
         }
@@ -66,7 +59,7 @@ def get_neighbors(entity):
     # set return type to JSON
     sparql.setReturnFormat(JSON)
 
-    # execcute sparql query and write result to results
+    # execute sparql query and write result to results
     results = sparql.query().convert()
 
     for result in results["results"]["bindings"]:
