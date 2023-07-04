fb52be267731d4368eae79cb289552ab9c016b9e
Debayan Banerjee <debayanin@gmail.com>
2023-07-04 09:19:22 +0000

    Print entities too

--- kgqa/streamlit/home.py
+++ kgqa/streamlit/home.py
@@ -3,6 +3,7 @@ import requests
 import sys
 
 host = sys.argv[1]
+default_questions = ["What is the Wikidata ID of Pablo G. Tahoces?", "Which publications did Loukmen Regainia write?", "The author Sameh S. Askar is primarily affiliated to which institution?", "List the venues in which Matt Rowe published papers in the last six years and the titles of these papers.", "Who are the co-authors of Kolosov, Kirill and where are they affiliated?", "Find the venue of the paper 'Consensus in non-commutative spaces' authored by Alain Sarlette."]
 
 def infer(question):
     url = "http://%s:5000/answer"%(host)  # Replace with the actual API endpoint URL
@@ -27,9 +28,20 @@ with st.form("form1"):
     submitted = st.form_submit_button("Submit")
     if submitted:
         response = infer(question)
-        st.write(response)
-
+        resp = response['output'][0]
+        if resp['entities']:
+            st.write("Entities:")
+            for ent in resp['entities']:
+                st.write(ent)
+        else:
+            st.write("No entities detected")
+        if resp['query']:
+            st.write("SPARQL Query:")
+            st.write(resp['query'])
+        if resp['answer']:
+            st.write("Answer:")
+            st.write(resp['answer'])
+        else:
+            st.write("No answer could be fetched")
     
-    
-
 
