126cf99aa1b808e387857cc8ab2670127b431d56
Debayan Banerjee <debayanin@gmail.com>
2023-07-04 16:01:48 +0000

    Added an About page

---
+++ kgqa/streamlit/pages/About.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("DBLP Scholarly Question Answering")
+st.write("This project is developed at the [HCDS Group](https://www.hcds.uni-hamburg.de/hcds.html) of the Universität Hamburg, Germany.")
+st.write("For issues, please contact debayan.banerjee@uni-hamburg.de.")
+
+
