45960405a54e905bef7744507330c239fba0245f
Debayan Banerjee <debayanin@gmail.com>
2023-07-04 16:53:50 +0000

    type in seid's name

--- kgqa/streamlit/pages/About.py
+++ kgqa/streamlit/pages/About.py
@@ -5,6 +5,6 @@ st.write("This project is developed at the [HCDS Group](https://www.hcds.uni-ham
 st.write("The dataset used to train the underlying QA model is [DBLP-QuAD](https://huggingface.co/datasets/awalesushil/DBLP-QuAD).")
 st.write("The DBLP dump being used in the backend may be used via a [SPARQL endpoint](https://dblp-kg.ltdemos.informatik.uni-hamburg.de/sparql).")
 st.write("This application is a sister-project of [ARDIAS](https://ardias.ltdemos.informatik.uni-hamburg.de/).")
-st.write("For issues, please contact debayan.banerjee@uni-hamburg.de or seid.mohie.yimam@uni-hamburg.de.")
+st.write("For issues, please contact debayan.banerjee@uni-hamburg.de or seid.muhie.yimam@uni-hamburg.de.")
 
 
