1c8b09babf2de2988ef495e12a4b7c9854235e0d
Debayan Banerjee <debayanin@gmail.com>
2023-07-13 15:55:26 +0200

    Adding complex and distmult to re-ranker

--- entitylinker/reranker/infer.py
+++ entitylinker/reranker/infer.py
@@ -31,13 +31,24 @@ class SiameseNetwork(nn.Module):
         output_vector = self.forward_once(input_vector)
         return output_vector
 
-es = Elasticsearch("http://ltcpu2:2200/")
-model = SiameseNetwork()
-model_path = sys.argv[1]
-model.load_state_dict(torch.load(model_path))
-model.eval()
 sentmodel = SentenceTransformer('bert-base-nli-mean-tokens')
+model = {}
+es = Elasticsearch("http://ltcpu2:2200/")
 
+model['transe'] = SiameseNetwork()
+model_transe_path = "model_transe_1/model_epoch_4.pth"
+model['transe'].load_state_dict(torch.load(model_transe_path))
+model['transe'].eval()
+
+model['distmult'] = SiameseNetwork()
+model_distmult_path = "model_distmult_1/model_epoch_5.pth"
+model['distmult'].load_state_dict(torch.load(model_distmult_path))
+model['distmult'].eval()
+
+model['complex'] = SiameseNetwork()
+model_complex_path = "model_complex_1/model_epoch_6.pth"
+model['complex'].load_state_dict(torch.load(model_complex_path))
+model['complex'].eval()
 
 def label_search_es(label, enttype):
     try:
@@ -53,9 +64,14 @@ def label_search_es(label, enttype):
         print(err)
         return []
 
-def fetchembedding(entid):
+def fetchembedding(entid,embedding):
     try:
-        resp = es.search(index="dblpembedstranseindex01", query={"match":{"key":entid}})
+        if embedding == 'transe':
+            resp = es.search(index="dblpembedstranseindex01", query={"match":{"key":entid}})
+        elif embedding == 'distmult':
+            resp = es.search(index="dblpembedsdistmultindex01", query={"match":{"key":entid}})
+        elif embedding == 'complex':
+            resp = es.search(index="dblpembedscomplexindex01", query={"match":{"key":entid}})
         #print(resp)
         embedding = [float(x) for x in resp['hits']['hits'][0]['_source']['embedding']]
         return embedding
@@ -64,14 +80,14 @@ def fetchembedding(entid):
         return []
 
 
-def link(question, label, entity_type):
+def link(question, label, entity_type, modeltype='transe'):
     candidate_entities_labels = label_search_es(label, entity_type)
-    candidate_embeddings = [fetchembedding(x[0]) for x in candidate_entities_labels]
+    candidate_embeddings = [fetchembedding(x[0], modeltype) for x in candidate_entities_labels]
     question_encoding = list(sentmodel.encode([question])[0])+ 201*[0.0]
-    question_embedding = model(torch.tensor(question_encoding))
+    question_embedding = model[modeltype](torch.tensor(question_encoding))
 
     candidate_encodings = [list(sentmodel.encode([x[1]])[0])+candidate_embeddings[idx]+[fuzz.token_set_ratio(x[1],question)/100.0]  for idx,x in enumerate(candidate_entities_labels)]
-    candidate_embeddings = [model(torch.tensor(x)) for x in candidate_encodings]
+    candidate_embeddings = [model[modeltype](torch.tensor(x)) for x in candidate_encodings]
     arr = []
     for idx,candidate_embedding in enumerate(candidate_embeddings):
         distance = torch.norm(question_embedding - candidate_embedding, p=2)
@@ -85,13 +101,13 @@ def link(question, label, entity_type):
 
 app = Flask(__name__)
 
-@app.route('/entitylinker', methods=['POST'])
-def process():
+@app.route('/entitylinker/<embedding>', methods=['POST'])
+def process(embedding):
     data = request.get_json()  # Get the JSON data from the request
     question = data['question']
     label = data['label']
     entity_type = data['type']
-    results = link(question, label, entity_type)
+    results = link(question, label, entity_type, embedding)
     return jsonify(results)
 
 if __name__ == '__main__':
