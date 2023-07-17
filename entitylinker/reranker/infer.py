19f556e042b89645184171f5e698f1daf6fb3e62
Debayan Banerjee <debayanin@gmail.com>
2023-07-17 17:31:14 +0200

    Docker working for dblplink

--- entitylinker/reranker/infer.py
+++ entitylinker/reranker/infer.py
@@ -111,5 +111,5 @@ def process(embedding):
     return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0',port=5001)
+    app.run(debug=False, host='0.0.0.0',port=5002)
 
