1f3f582638cf47402b179937f24f760e42ad961b
Debayan Banerjee <debayanin@gmail.com>
2023-07-19 10:33:50 +0200

    Remove duplicates from final reranker list

--- entitylinker/reranker/infer.py
+++ entitylinker/reranker/infer.py
@@ -50,6 +50,11 @@ model_complex_path = "model_complex_1/model_epoch_6.pth"
 model['complex'].load_state_dict(torch.load(model_complex_path))
 model['complex'].eval()
 
+def remove_duplicates(array_of_arrays):
+    unique_arrays = set((inner_array[0],inner_array[1][0],inner_array[1][1]) for inner_array in array_of_arrays)
+    return [list(unique_array) for unique_array in unique_arrays]
+
+
 def label_search_es(label, enttype):
     try:
         enttypes = ["https://dblp.org/rdf/schema#"+x for x in enttype]
@@ -92,6 +97,7 @@ def link(question, label, entity_type, modeltype='transe'):
     for idx,candidate_embedding in enumerate(candidate_embeddings):
         distance = torch.norm(question_embedding - candidate_embedding, p=2)
         arr.append([distance.item(),candidate_entities_labels[idx]])
+    arr = remove_duplicates(arr)
     sorted_entities =  sorted(arr, key=lambda d: d[0])
     print(sorted_entities)
     return sorted_entities
